9186838d558e764d01e89d585c66da522057b789
KumaTea <36222458+oudoubleyang@users.noreply.github.com>
2019-05-25 10:59:09 +0800

    bug fix

--- msgtype.py
+++ msgtype.py
@@ -10,24 +10,19 @@ def msgtype(data):
         if 'message' in data:
             if 'new_chat_member' in data['message']:
                 resp = grpnewmem(data)
-                return resp
             elif 'text' in data['message']:
                 resp = grptext(data)
-                return resp
             elif 'sticker' in data['message']:
                 resp = grpsticker(data)
-                return resp
             elif 'photo' in data['message']:
                 resp = grpphoto(data)
-                return resp
             elif 'video' in data['message']:
                 resp = grpvideo(data)
-                return resp
             elif 'document' in data['message']:
                 resp = grpfile(data)
-                return resp
             elif 'edited_message' in data:
-                return 'ignore edited message.'
+                resp = 'ignore edited message.'
+            return resp
         elif 'channel_post' or 'edited_channel_post' in data:
             return 'Channel post'
         elif 'left_chat_member' in data:
@@ -39,22 +34,17 @@ def msgtype(data):
         if 'message' in data:
             if 'text' in data['message']:
                 resp = privtext(data)
-                return resp
             elif 'sticker' in data['message']:
                 resp = privsticker(data)
-                return resp
             elif 'photo' in data['message']:
                 resp = privphoto(data)
-                return resp
             elif 'video' in data['message']:
                 resp = privvideo(data)
-                return resp
             elif 'document' in data['message']:
                 resp = privfile(data)
-                return resp
             elif 'sticker' in data['message']:
                 resp = privsticker(data)
-                return resp
+            return resp
         elif 'edited_message' in data:
             return 'ignore edited message.'
 
