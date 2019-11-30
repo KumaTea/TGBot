6824c023660b992e23be20ead999b15e6a61f024
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-11-30 23:47:35 +0800

    2.1.0.1

--- mdNewMember.py
+++ mdNewMember.py
@@ -1,9 +1,9 @@
 from botSession import bot
-from botDb import welcome_msg
+from localDb import welcome_msg
 from threading import Timer
 
 
-def welcome(chat_id, user_id, new_msg_id):
+def welcome(chat_id, user_id, system_new_mem_msg_id):
     if chat_id in welcome_msg:
         resp = 'Familiar'
         if 'message' in welcome_msg[chat_id]:
@@ -16,14 +16,16 @@ def welcome(chat_id, user_id, new_msg_id):
             sticker_id = bot.get(sticker).message('id')
         else:
             sticker_id = None
-        check = Timer(5, check_member, [chat_id, user_id, new_msg_id, msg_id, sticker_id])  # Ignore PyCharm Error
+        check = Timer(300, check_member, [chat_id, user_id, system_new_mem_msg_id, msg_id, sticker_id])
+        # Ignore PyCharm Error
         check.start()
     else:
         resp = 'Not familiar using default'
     return resp
 
 
-def check_member(chat_id, user_id, new_msg_id, msg_id=None, sticker_id=None):
+def check_member(chat_id, user_id, system_new_mem_msg_id, msg_id=None, sticker_id=None):
+    print(f'[INFO] Starting new member checking...')
     left = False
     try:
         user = bot.query(chat_id).chat_member(user_id, raw=True)
@@ -33,9 +35,12 @@ def check_member(chat_id, user_id, new_msg_id, msg_id=None, sticker_id=None):
         left = True
 
     if left:
-        bot.delete(chat_id).message(new_msg_id)
+        bot.delete(chat_id).message(system_new_mem_msg_id)
         if sticker_id:
             bot.delete(chat_id).message(sticker_id)
         if msg_id:
             bot.edit(chat_id, msg_id).message('验证机器人已移除一位未通过验证的用户。')
+        print(f'[INFO] User {user_id} status: LEFT; NOT member.')
+    else:
+        print(f'[INFO] User {user_id} status: IN; IS member.')
     return True
