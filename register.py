27d82eb3b198ecc5e2093228487009d64674a703
KumaTea <36222458+KumaTea@users.noreply.github.com>
2021-01-10 05:15:09 +0800

    fix deprecated api

--- register.py
+++ register.py
@@ -9,13 +9,13 @@ def register_handlers():
 
     dp.add_handler(CommandHandler(['debug', 'dump'], debug))
     dp.add_handler(CommandHandler(['delay', 'ping'], delay))
-    dp.add_handler(CommandHandler(['rp', 'repeat'], repeat, Filters.group))
+    dp.add_handler(CommandHandler(['rp', 'repeat'], repeat, Filters.chat_type.groups))
 
-    dp.add_handler(CommandHandler('start', private_start, Filters.private))
-    dp.add_handler(CommandHandler(['fw', 'forward'], private_forward, Filters.private))
-    dp.add_handler(CommandHandler('help', private_help, Filters.private))
+    dp.add_handler(CommandHandler('start', private_start, Filters.chat_type.private))
+    dp.add_handler(CommandHandler(['fw', 'forward'], private_forward, Filters.chat_type.private))
+    dp.add_handler(CommandHandler('help', private_help, Filters.chat_type.private))
 
-    dp.add_handler(MessageHandler((Filters.command & Filters.private), private_unknown))
+    dp.add_handler(MessageHandler((Filters.command & Filters.chat_type.private), private_unknown))
 
-    dp.add_handler(MessageHandler(Filters.private, private_get_file_id))
+    dp.add_handler(MessageHandler(Filters.chat_type.private, private_get_file_id))
     return True
