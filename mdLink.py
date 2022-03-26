c6930eb117a6c60442b3696f4ee966db239be802
KumaTea <KumaTea@outlook.com>
2022-03-27 01:06:37 +0800

    add screenshot notification

--- mdLink.py
+++ mdLink.py
@@ -1,8 +1,11 @@
+from random import choice
 from botSession import kuma
 from botInfo import creator
 from botTools import find_url
+from botDB import loading_image
 from mdNGA import nga_link_process
 from mdScreen import get_screenshot
+from telegram import InputMediaPhoto
 from mdWeibo import weibo_link_process
 
 try:
@@ -31,14 +34,20 @@ def look(update, context):
     command = message.text
     content_index = command.find(' ')
     if content_index == -1:
-        return None
+        return kuma.send_message(chat_id, 'No content.')
     else:
         text = command[content_index + 1:]
         url = find_url(text)
         if not url:
-            return None
+            return kuma.send_message(chat_id, 'No url.')
+
+        inform = kuma.send_photo(chat_id, choice(loading_image), caption='Getting screenshot...')
+        inform_id = inform.message_id
         try:
+            kuma.send_chat_action(chat_id, 'upload_photo')
             screenshot = get_screenshot(url)
-            return kuma.send_photo(chat_id, screenshot)
+            kuma.edit_message_media(chat_id, inform_id, media=InputMediaPhoto(screenshot))
+            # kuma.edit_message_caption(chat_id, inform_id, caption='')
+            return True
         except Exception as e:
-            return kuma.send_message(chat_id, 'Error: {}'.format(e))
+            return kuma.edit_message_caption(chat_id, inform_id, 'Error: {}'.format(e))
