e194d894f3856815122c73afe9cc3227294271c3
KumaTea <KumaTea@outlook.com>
2022-04-17 17:10:48 +0800

    scripts minor fix

--- docker/create-env.sh
+++ docker/create-env.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
+source /root/.bashrc > /dev/null
 export CONDA_EXE=/opt/conda/bin/conda
 $CONDA_EXE create -n tg python=3.9 pip setuptools wheel flask requests gunicorn selenium python-telegram-bot -y
-conda activate tg > /dev/null
+# $CONDA_EXE activate tg > /dev/null
 
 export CONDA_TG=/opt/conda/envs/tg/bin/python3
 $CONDA_TG -m pip install -U python-telegram-bot
