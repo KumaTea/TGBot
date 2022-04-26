360bead2e2c4de577f09a4b36d4459c6f5c9b631
KumaTea <KumaTea@outlook.com>
2022-04-27 01:10:42 +0800

    Hotfix for Pyrogram 2.0

--- register.py
+++ register.py
@@ -11,23 +11,23 @@ from pyrogram.handlers import MessageHandler
 
 def register_handlers():
     # group commands
-    kuma.add_handler(MessageHandler(repeat, filters.command(['rp', 'repeat']) & filters.group & ~filters.edited))
-    kuma.add_handler(MessageHandler(title, filters.command(['title', 'entitle']) & filters.group & ~filters.edited))
+    kuma.add_handler(MessageHandler(repeat, filters.command(['rp', 'repeat']) & filters.group))  # & ~filters.edited))
+    kuma.add_handler(MessageHandler(title, filters.command(['title', 'entitle']) & filters.group))  # & ~filters.edited))
 
     # private commands
-    kuma.add_handler(MessageHandler(private_start, filters.command(['start']) & filters.private & ~filters.edited))
-    kuma.add_handler(MessageHandler(private_forward, filters.command(['fw', 'forward']) & filters.private & ~filters.edited))
-    kuma.add_handler(MessageHandler(private_help, filters.command(['help']) & filters.private & ~filters.edited))
+    kuma.add_handler(MessageHandler(private_start, filters.command(['start']) & filters.private))  # & ~filters.edited))
+    kuma.add_handler(MessageHandler(private_forward, filters.command(['fw', 'forward']) & filters.private))  # & ~filters.edited))
+    kuma.add_handler(MessageHandler(private_help, filters.command(['help']) & filters.private))  # & ~filters.edited))
 
     # universal commands
-    kuma.add_handler(MessageHandler(debug, filters.command(['debug', 'dump']) & ~filters.edited))
-    kuma.add_handler(MessageHandler(delay, filters.command(['delay', 'ping']) & ~filters.edited))
-    kuma.add_handler(MessageHandler(look, filters.command(['look', 'get', 'screenshot']) & ~filters.edited))
+    kuma.add_handler(MessageHandler(debug, filters.command(['debug', 'dump'])))  # & ~filters.edited))
+    kuma.add_handler(MessageHandler(delay, filters.command(['delay', 'ping'])))  # & ~filters.edited))
+    kuma.add_handler(MessageHandler(look, filters.command(['look', 'get', 'screenshot'])))  # & ~filters.edited))
 
-    kuma.add_handler(MessageHandler(private_get_file_id, filters.private & ~filters.edited))
+    kuma.add_handler(MessageHandler(private_get_file_id, filters.private))  # & ~filters.edited))
 
-    kuma.add_handler(MessageHandler(process_msg, filters.group & ~filters.edited))
-    kuma.add_handler(MessageHandler(private_unknown, filters.private & ~filters.edited))
+    kuma.add_handler(MessageHandler(process_msg, filters.group))  # & ~filters.edited))
+    kuma.add_handler(MessageHandler(private_unknown, filters.private))  # & ~filters.edited))
 
     return logging.info('Registered handlers')
 
