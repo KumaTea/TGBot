87d9419d2e93ef058955427b43b110ee9ce281b4
KumaTea <KumaTea@outlook.com>
2022-08-12 00:25:27 +0800

    add hash to random seed

--- process_msg.py
+++ process_msg.py
@@ -1,6 +1,7 @@
 import re
+import random
+import hashlib
 from session import kuma
-from random import randint
 from localDb import trusted_group
 from tools import mention_other_bot
 
@@ -13,11 +14,14 @@ except ImportError:
 
 def douban_mark(message):
     title_re = r'《.+》'
-    text = message.text
+    text = message.text or message.caption
     chat_id = message.chat.id
     result = re.findall(title_re, text)
     if result:
-        mark = randint(10, 100)
+        title = result[0][1:-1].strip().lower()
+        title_hash = int(hashlib.md5(title.encode("utf-8")).hexdigest(), 16)
+        random.seed(title_hash)
+        mark = random.randint(10, 100)
         mark_str = str(mark)[:-1] + '.' + str(mark)[-1:]
         text = f'豆瓣评分：{mark_str}'
         return kuma.send_message(chat_id, text)
@@ -32,12 +36,13 @@ def process_msg(client, message):
     if message:
         text = message.text or message.caption
         if text:
-            user_id = message.from_user.id
-            if user_id > 0:
-                if not mention_other_bot(text):
-                    chat_id = message.chat.id
-                    if chat_id in trusted_group:
-                        return public_process(message) or local_process(message)
-                    else:
-                        return public_process(message)
+            if message.from_user:
+                user_id = message.from_user.id
+                if user_id > 0:
+                    if not mention_other_bot(text):
+                        chat_id = message.chat.id
+                        if chat_id in trusted_group:
+                            return public_process(message) or local_process(message)
+                        else:
+                            return public_process(message)
     return None
